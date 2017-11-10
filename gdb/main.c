2690b6df12941bd8337f64188ae6da52b81545bd
Jeff Mahoney <jeffm@suse.com>
2017-11-10 11:56:39 -0500

    gdb-6.6-buildid-locate-core-as-arg.patch

--- gdb/main.c
+++ gdb/main.c
@@ -459,6 +459,37 @@ struct cmdarg
   char *string;
 };
 
+/* Call exec_file_attach.  If it detected FILENAME is a core file call
+   core_file_command.  Print the original exec_file_attach error only if
+   core_file_command failed to find a matching executable.  */
+
+static void
+exec_or_core_file_attach (const char *filename, int from_tty)
+{
+  volatile struct gdb_exception e;
+
+  gdb_assert (exec_bfd == NULL);
+
+  TRY
+    {
+      exec_file_attach (filename, from_tty);
+    }
+  CATCH (e, RETURN_MASK_ALL)
+    {
+      if (e.error == IS_CORE_ERROR)
+	{
+	  core_file_command ((char *) filename, from_tty);
+
+	  /* Iff the core file found its executable suppress the error message
+	     from exec_file_attach.  */
+	  if (exec_bfd != NULL)
+	    return;
+	}
+      throw_exception (e);
+    }
+  END_CATCH
+}
+
 static void
 captured_main_1 (struct captured_main_args *context, int &python_script)
 {
@@ -912,6 +943,8 @@ captured_main_1 (struct captured_main_args *context, int &python_script)
 	{
 	  symarg = argv[optind];
 	  execarg = argv[optind];
+	  if (optind + 1 == argc && corearg == NULL)
+	    corearg = argv[optind];
 	  optind++;
 	}
 
@@ -1062,11 +1095,25 @@ captured_main_1 (struct captured_main_args *context, int &python_script)
       && symarg != NULL
       && strcmp (execarg, symarg) == 0)
     {
+      catch_command_errors_const_ftype *func;
+
+      /* Call exec_or_core_file_attach only if the file was specified as
+	 a command line argument (and not an a command line option).  */
+      if (corearg != NULL && strcmp (corearg, execarg) == 0)
+	{
+	  func = exec_or_core_file_attach;
+	  corearg = NULL;
+	}
+      else
+	func = exec_file_attach;
+
       /* The exec file and the symbol-file are the same.  If we can't
          open it, better only print one error message.
-         catch_command_errors returns non-zero on success!  */
-      if (catch_command_errors_const (exec_file_attach, execarg,
-				      !batch_flag))
+         catch_command_errors returns non-zero on success!
+	 Do not load EXECARG as a symbol file if it has been already processed
+	 as a core file.  */
+      if (catch_command_errors_const (func, execarg, !batch_flag)
+	  && core_bfd == NULL)
 	catch_command_errors_const (symbol_file_add_main_adapter, symarg,
 				    !batch_flag);
     }
